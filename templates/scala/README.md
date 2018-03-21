1360c1f40cbc848dd7d1b7c717b85b40a05e2a8b
Nico Serfontein <nserfontein@gmail.com>
2018-03-21 06:15:23 +0200

    Update

---
+++ templates/scala/README.md
@@ -0,0 +1,2 @@
+# WARNING: Hanging
+- `[info] Updating ProjectRef(uri("file:/home/vagrant/hello/project/"), "hello-build")...`
--- templates/scala/provision/sbt.sh
+++ templates/scala/provision/sbt.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt-get update
